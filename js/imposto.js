db56ff25bfdb23457ef3d1c341484ab41dd9c3b2
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-10-23 06:33:46 -0300

    desafio 2 e 3

---
+++ js/imposto.js
@@ -0,0 +1,48 @@
+var sal = 3002.00;
+aux = 20;
+
+console.log(aux);
+// if (sal <=2000){
+//     console.log('Isento')
+// }else if(sal<= 3000){
+//     let segFaixa = (sal-2000)*0.08;
+//     let result = segFaixa.toFixed(2);
+//     console.log('R$' + result);
+// }else if( sal <=4500){
+//     let faixaC1 = sal - 2000;
+//     let faixaC2 = sal - 3000;
+// }
+
+
+// let calc = 3002.00;
+// calc = parseFloat(calc);
+
+// if (calc<=2000){
+//   console.log('Isento');
+// } else if (calc<=3000) {
+//   segundaFaixa = (calc - 2000)*0.08;
+//   result = parseFloat(segundaFaixa);
+//   result = result.toFixed(2);
+//   console.log('R$ ' + result);
+// } else if (calc<=4500) {
+//     aux1 = calc - 2000;
+//     aux2 = calc - 3000;
+//     aux2 = aux2>=0 ? aux2 : 0;
+//     segundaFaixa = ((calc - 2000)-aux2)* 0.08;
+//     terceiraFaixa = (calc - 3000) * 0.18;
+//     result = parseFloat(segundaFaixa+terceiraFaixa);
+//     result = result.toFixed(2);
+//       console.log('R$ ' + result);
+// } else {
+//     aux1 = calc - 2000;
+//     aux2 = calc - 3000;
+//     aux3 = calc - 4500;
+//     aux2 = aux2>=0 ? aux2 : 0;
+//     aux3 = aux3>=0 ? aux3 : 0;
+//     segundaFaixa = ((calc - 2000)-aux2)* 0.08;
+//     terceiraFaixa = ((calc - 3000)-aux3) * 0.18;
+//     quartaFaixa =  (calc-4500) * 0.28;
+//     result = parseFloat(segundaFaixa+terceiraFaixa+quartaFaixa);
+//     result = result.toFixed(2);
+//    console.log('R$ ' + result);
+// }
--- js/script.js
+++
@@ -1,5 +0,0 @@
-var tempo = 10;
-var velo = 85;
-var resultado = (tempo * velo)/12;
-
-console.log(resultado.toFixed(3));
