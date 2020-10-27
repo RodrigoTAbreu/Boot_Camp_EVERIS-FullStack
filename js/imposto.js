375d34fd312897d7712c9f33e41b544e496890b4
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-10-27 03:15:19 -0300

    imposto

--- js/imposto.js
+++ js/imposto.js
@@ -1,48 +1,15 @@
-var sal = 3002.00;
-aux = 20;
+let salario = 3002.00;
+let calc = salario - 2000.00;
 
-console.log(aux);
-// if (sal <=2000){
-//     console.log('Isento')
-// }else if(sal<= 3000){
-//     let segFaixa = (sal-2000)*0.08;
-//     let result = segFaixa.toFixed(2);
-//     console.log('R$' + result);
-// }else if( sal <=4500){
-//     let faixaC1 = sal - 2000;
-//     let faixaC2 = sal - 3000;
-// }
-
-
-// let calc = 3002.00;
-// calc = parseFloat(calc);
-
-// if (calc<=2000){
-//   console.log('Isento');
-// } else if (calc<=3000) {
-//   segundaFaixa = (calc - 2000)*0.08;
-//   result = parseFloat(segundaFaixa);
-//   result = result.toFixed(2);
-//   console.log('R$ ' + result);
-// } else if (calc<=4500) {
-//     aux1 = calc - 2000;
-//     aux2 = calc - 3000;
-//     aux2 = aux2>=0 ? aux2 : 0;
-//     segundaFaixa = ((calc - 2000)-aux2)* 0.08;
-//     terceiraFaixa = (calc - 3000) * 0.18;
-//     result = parseFloat(segundaFaixa+terceiraFaixa);
-//     result = result.toFixed(2);
-//       console.log('R$ ' + result);
-// } else {
-//     aux1 = calc - 2000;
-//     aux2 = calc - 3000;
-//     aux3 = calc - 4500;
-//     aux2 = aux2>=0 ? aux2 : 0;
-//     aux3 = aux3>=0 ? aux3 : 0;
-//     segundaFaixa = ((calc - 2000)-aux2)* 0.08;
-//     terceiraFaixa = ((calc - 3000)-aux3) * 0.18;
-//     quartaFaixa =  (calc-4500) * 0.28;
-//     result = parseFloat(segundaFaixa+terceiraFaixa+quartaFaixa);
-//     result = result.toFixed(2);
-//    console.log('R$ ' + result);
-// }
+if (salario <=2000.00){
+    console.log('Isento');
+}else if(salario >= 2000.01 && salario <= 3000.00) {
+    var valor1 = calc - 2000;
+    var valor2 = calc - 3000;
+    var resultFaixa01 = valor1 * 0.08;
+    var resultFaixa02 = valor2 * 0.18;
+}
+console.log(`${typeof resultFaixa01} e ${typeof resultFaixa02}`);
+console.log(calc);
+console.log(valor1);
+console.log(valor2);
